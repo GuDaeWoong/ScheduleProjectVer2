36920bf53069fb70af7b271a34fc0af197bb0e10
GuDaeWoong <39939767+GuDaeWoong@users.noreply.github.com>
2025-05-26 19:42:11 +0900

    Fix : Author update 수정

--- src/main/java/com/example/scheduleprojectver2/lv4/dto/author/AuthorUpdateRequestDto.java
+++ src/main/java/com/example/scheduleprojectver2/lv4/dto/author/AuthorUpdateRequestDto.java
@@ -1,7 +1,6 @@
 package com.example.scheduleprojectver2.lv4.dto.author;
 
 import jakarta.validation.constraints.AssertTrue;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -10,14 +9,11 @@ import lombok.Getter;
 public class AuthorUpdateRequestDto {
     private Long id;
 
-    @NotBlank
     @Size(max = 4, message = "사용자명은 4글자를 초과할 수 없습니다.")
     private String name;
 
-    @NotBlank
     private String password;
 
-    @NotBlank
     @Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])+[.][a-zA-Z]{2,3}$", message = "이메일 주소 양식을 확인해주세요")
     private String email;
 
